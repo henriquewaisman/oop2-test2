3bb6b94daac5fbd623662b90f5d0b516e6ad75bf
henriquewaisman <100635335+henriquewaisman@users.noreply.github.com>
2023-11-17 10:06:19 -0300

    Create leilaoRoutes.js

---
+++ routes/leilaoRoutes.js
@@ -0,0 +1,9 @@
+// routes/leilaoRoutes.js
+const express = require('express');
+const router = express.Router();
+const LeilaoController = require('../controllers/LeilaoController');
+
+router.post('/leiloes', LeilaoController.criarLeilao);
+router.get('/leiloes', LeilaoController.listarLeiloes);
+
+module.exports = router;
