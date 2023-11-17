02df2a1aff62daf79075aa9fb35c1baa46813523
henriquewaisman <100635335+henriquewaisman@users.noreply.github.com>
2023-11-17 10:06:32 -0300

    Create usuarioRoutes.js

---
+++ routes/usuarioRoutes.js
@@ -0,0 +1,9 @@
+// routes/usuarioRoutes.js
+const express = require('express');
+const router = express.Router();
+const UsuarioController = require('../controllers/UsuarioController');
+
+router.post('/usuarios', UsuarioController.criarUsuario);
+router.get('/usuarios', UsuarioController.listarUsuarios);
+
+module.exports = router;
