d56f99eed923383bac52e69c3245c4ba8cd0caef
henriquewaisman <100635335+henriquewaisman@users.noreply.github.com>
2023-11-17 10:06:07 -0300

    Create lanceRoutes.js

---
+++ routes/lanceRoutes.js
@@ -0,0 +1,9 @@
+// routes/lanceRoutes.js
+const express = require('express');
+const router = express.Router();
+const LanceController = require('../controllers/LanceController');
+
+router.post('/lances', LanceController.criarLance);
+router.get('/lances', LanceController.listarLances);
+
+module.exports = router;
