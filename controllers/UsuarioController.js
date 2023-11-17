87f9f2b9bb98e2bcda5cf7bcc6cf55abaf3443f4
henriquewaisman <100635335+henriquewaisman@users.noreply.github.com>
2023-11-17 10:22:30 -0300

    Update UsuarioController.js

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,16 +1,15 @@
 // controllers/UsuarioController.js
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const UsuarioService = require('../services/UsuarioService');
 
 module.exports = {
   async criarUsuario(req, res) {
     const { nome, email } = req.body;
-    const usuario = await prisma.usuario.create({ data: { nome, email } });
+    const usuario = await UsuarioService.criarUsuario(nome, email);
     res.json(usuario);
   },
 
   async listarUsuarios(req, res) {
-    const usuarios = await prisma.usuario.findMany();
+    const usuarios = await UsuarioService.listarUsuarios();
     res.json(usuarios);
   },
 };
